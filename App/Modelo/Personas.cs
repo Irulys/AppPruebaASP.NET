1f7f98c8de8abf7e59d80c27f28bad31bc2c50a5
irulys <lipe21@hotmail.es>
2015-03-02 18:57:45 -0500

    Hoy

---
+++ App/Modelo/Personas.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Personas
+    {
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string nombre;
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        private string nombre;
+
+        public string nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private string email;
+
+        public string email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+     
+        public Personas 
+        {
+
+
+
+    }
+
+
+    }
+}
